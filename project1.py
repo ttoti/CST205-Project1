04d6cb6820f0299c429c6c0e8c26aefe40c0ef2c
Tomas Hernandez <tohernandez@csumb.edu>
2016-09-08 09:41:18 -0700

    Changed variable names for better understanding

--- project1.py
+++ project1.py
@@ -25,32 +25,33 @@ def open_files(dir):
 
 #Returns array of the median pixel values for images at pixel x & y
 def getMedianRGB(x,y):
-    array = []
-    returnArray = []
-    #Converts images to RGB to get pixel data then add to array for later use
+    rgbArray = []
+    medianRGB = []
+    #Converts images to RGB to get pixel data then add to rgbArray for later use
     for key, value in dictionary.items():
         rgb_im = dictionary[key].convert('RGB')
         red, green, blue = rgb_im.getpixel((x,y))
-        array.append([red, green, blue])
+        rgbArray.append([red, green, blue])
         mediun_dictionary[key] = red + green + blue
 
-    sorted_dict = sorted(mediun_dictionary.items(), key=operator.itemgetter(1))
+    sortedMediumDictionary = sorted(mediun_dictionary.items(), key=operator.itemgetter(1))
     for x in range(0, numberOfFiles - 1):
-        if(sorted_dict[int(numberOfFiles / 2) - 1][1] == (array[x][0] + array[x][1] + array[x][2])):
-           returnArray = (array[x])
-    return returnArray
+        if(sortedMediumDictionary[int(numberOfFiles / 2) - 1][1] == (rgbArray[x][0] + rgbArray[x][1] + rgbArray[x][2])):
+           medianRGB = (rgbArray[x])
+    return medianRGB
 
 #Main
 open_files(DIR)
-img = Image.new( 'RGB', (dictionary["image1"].size[0] - 1, dictionary["image1"].size[1] - 1), "Black")
-pixels = img.load()
 xSize = int((dictionary["image1"].size[0] - 1))
 ySize = int((dictionary["image1"].size[1] - 1))
+img = Image.new( 'RGB', (xSize, ySize), "White")
+pixels = img.load()
 #Loops through all pixels by using X and Y
 for x in range(0, xSize):
     for y in range(0, ySize):
        rgb = (getMedianRGB(x,y))
        pixels[x,y] = (rgb[0], rgb[1], rgb[2])
+       #Printing out to verify
        print(str(x) + " " + str(y))
 img.save("result.jpg", 'jpeg')
 img.show()
